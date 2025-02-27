876f9ffa98d4fbb86864361b471fda205420c1ec
peti12352 <64523372+peti12352@users.noreply.github.com>
2025-02-27 16:52:55 +0100

    improved base site4

--- js/script.js
+++ js/script.js
@@ -12,22 +12,42 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  // Highlight the active nav link based on the current scroll position
-  const sections = document.querySelectorAll("section");
-  window.addEventListener("scroll", () => {
-    let currentSectionId = "";
-    sections.forEach((section) => {
-      const sectionTop = section.offsetTop - 100; // offset for navbar height
-      if (window.pageYOffset >= sectionTop) {
-        currentSectionId = section.getAttribute("id");
-      }
-    });
+  // Debounce function to improve scroll event performance
+  function debounce(func, wait = 20, immediate = true) {
+    let timeout;
+    return function () {
+      const context = this,
+        args = arguments;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
 
-    navLinks.forEach((link) => {
-      link.classList.remove("active");
-      if (link.getAttribute("href") === "#" + currentSectionId) {
-        link.classList.add("active");
-      }
-    });
-  });
+  // Highlight active navigation link based on current scroll position
+  const sections = document.querySelectorAll("section");
+  window.addEventListener(
+    "scroll",
+    debounce(() => {
+      let currentSectionId = "";
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop - 100; // offset for navbar height
+        if (window.pageYOffset >= sectionTop) {
+          currentSectionId = section.getAttribute("id");
+        }
+      });
+
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + currentSectionId) {
+          link.classList.add("active");
+        }
+      });
+    })
+  );
 });
